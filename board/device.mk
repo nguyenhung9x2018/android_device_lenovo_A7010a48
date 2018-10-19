7b856aa99cf8c0de987b5fc18482dd818340be7d
Sahil Sonar <43287494+SahilSonar@users.noreply.github.com>
2018-10-19 22:04:18 +0530

    Huge Update

---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := k5fpr,A7010a48,LENOVO,a7010a48
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
