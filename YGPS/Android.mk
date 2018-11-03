199aefa2b8dbd0466282cb2c6eedcdf985ca83fd
BunsExynos <himanshuakela@gmail.com>
2018-11-03 23:24:33 +0530

    k5fpr : add local sdk version in YGPS for pie

--- YGPS/Android.mk
+++ YGPS/Android.mk
@@ -11,6 +11,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := YGPS
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
 
