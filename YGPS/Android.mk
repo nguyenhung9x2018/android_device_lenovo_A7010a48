fe69f39f01db2065555848150f81a6c8d8a9406c
Sahil Sonar <43287494+SahilSonar@users.noreply.github.com>
2018-11-04 23:00:19 +0530

    Added SDK version !

--- YGPS/Android.mk
+++ YGPS/Android.mk
@@ -11,7 +11,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := YGPS
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
-
+LOCAL_SDK_VERSION := current
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
